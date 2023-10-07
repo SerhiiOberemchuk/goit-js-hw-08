3f4ad7742d0cb5acba4738584a85190de221fa92
SerhiiOberemchuk <gardenitparts@gmail.com>
2023-10-07 22:02:59 +0200

    task03-01

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,28 @@
+import throttle from 'lodash.throttle';
 
+const formInput = document.querySelector('.feedback-form');
+console.dir(formInput);
+
+function checkLocalStorage(params) {
+  localStorage.getItem();
+}
+
+const feedback_form = {};
+const inputTextTrottle = throttle(inputText, 500);
+formInput.addEventListener('input', inputTextTrottle);
+
+function inputText(event) {
+  feedback_form[event.target.name] = event.target.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(feedback_form));
+  // console.dir(event.target.name);
+}
+console.log(feedback_form);
+
+formInput.addEventListener('submit', clickOnSubmit);
+function clickOnSubmit(evt) {
+  evt.preventDefault();
+  console.log(feedback_form);
+  evt.target.reset();
+  localStorage.removeItem('feedback-form-state');
+}
+console.log(feedback_form);
