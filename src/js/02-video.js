aefe58c301eb852a26b20c4323219d25b96022ed
SerhiiOberemchuk <gardenitparts@gmail.com>
2023-10-07 19:23:37 +0200

    task-02-02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,7 +1,7 @@
-import Vimeo from '@vimeo/player';
 import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
 const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
+const player = new Player(iframe);
 
 const onPlay = function (data) {
   localStorage.setItem('videoplayer-current-time', data.seconds);
