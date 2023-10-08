c7c52a994457b877ee9af6720caa97a5ca6111ae
SerhiiOberemchuk <gardenitparts@gmail.com>
2023-10-08 10:31:47 +0200

    task03-03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,22 +1,13 @@
 import throttle from 'lodash.throttle';
 
 const formInput = document.querySelector('.feedback-form');
-console.dir(formInput);
 
 function checkLocalStorage() {
   const userdata = JSON.parse(localStorage.getItem('feedback-form-state'));
-  console.log(userdata);
   if (userdata !== null) {
-    console.log('we have data');
-
     for (const key in userdata) {
       formInput.elements[key].value = userdata[key];
-      console.log(userdata[key]);
-      console.log(key);
-      console.log(formInput.elements[key]);
     }
-  } else {
-    console.log('local storage is empty');
   }
 }
 checkLocalStorage();
@@ -29,7 +20,6 @@ function inputText(event) {
   clientData[event.target.name] = event.target.value;
   localStorage.setItem('feedback-form-state', JSON.stringify(clientData));
 }
-// console.log(clientData);
 
 formInput.addEventListener('submit', clickOnSubmit);
 function clickOnSubmit(evt) {
@@ -38,4 +28,3 @@ function clickOnSubmit(evt) {
   evt.target.reset();
   localStorage.removeItem('feedback-form-state');
 }
-// console.log(clientData);
